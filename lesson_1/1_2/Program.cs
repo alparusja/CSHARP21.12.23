9a0a7a2a3cc75ede3dcb27392517d499c14cfbae
alparusja <alparusja@mail.ru>
2023-12-21 21:40:21 +0300

    lesson_1\1_2 Условие

--- lesson_1/1_2/Program.cs
+++ lesson_1/1_2/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿// Напишите программу, которая на вход принимает одно число
+//    (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Enter a number 1: ");
+int b1 = int.Parse(Console.ReadLine()!);
+int count = 0;
+
+while (count != b1) 
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("No");
