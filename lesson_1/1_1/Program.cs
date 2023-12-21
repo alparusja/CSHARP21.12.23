d01a1289eb530cd3d773b890e0600a08ab315ba1
alparusja <alparusja@mail.ru>
2023-12-21 21:27:27 +0300

    lesson_1\1_1

--- lesson_1/1_1/Program.cs
+++ lesson_1/1_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет,
+//    является ли первое число квадратом второго.
+// Console.WriteLine("Введите первое число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter a number 1: ");
+int b1 = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Enter a number 2: ");
+int b2 = Convert.ToInt32(Console.ReadLine()!);
+
+if (b2 * b2 == b1) 
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("No");
