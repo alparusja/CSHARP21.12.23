dbeaf339af5729abd35f6e5960ebd5199876f142
alparusja <alparusja@mail.ru>
2023-12-25 21:04:50 +0300

    add Program.cs 2_3

--- lesson_2/2_3/Program.cs
+++ lesson_2/2_3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Напишите программу, которая будет принимать на вход два
+//    числа и выводить, является ли второе число кратным первому.
+//    Если второе число некратно первому, то программа выводит
+//    остаток от деления.
+Console.WriteLine("Впишите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Впишите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a % b == 0)
+{
+    Console.WriteLine("Кратно");;
+}
+else
+{
+
+    Console.WriteLine("Не кратно, остаток " + a % b);
+}
