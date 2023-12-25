c0f0b25faab2884700f910813760dee92105a041
alparusja <alparusja@mail.ru>
2023-12-25 20:33:12 +0300

    add 2_1 Program.cs

--- lesson_2/2_1/Program.cs
+++ lesson_2/2_1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая принимает на вход
+//    трёхзначное число и удаляет вторую цифру этого числа.
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine(a / 100 * 10 + a % 10);
