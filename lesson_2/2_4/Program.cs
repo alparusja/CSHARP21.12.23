98aafd99f9ec1833d6a40314686387e84690f585
alparusja <alparusja@mail.ru>
2023-12-25 21:18:24 +0300

    add Program.cs 2_4

--- lesson_2/2_4/Program.cs
+++ lesson_2/2_4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 4. Напишите программу, которая выводит третью с конца цифру
+//    заданного числа или сообщает что третьей цифры нет.
+Console.WriteLine("Впишите число: ");
+int a = int.Parse(Console.ReadLine()!);
+if (a >= 100)
+{
+    int a1 = a / 100 % 10;
+    Console.WriteLine(a1);
+}
+else
+{
+    if (a < 100)
+    {
+        Console.WriteLine("Третьей цифры в числе нет");
+    }
+}
