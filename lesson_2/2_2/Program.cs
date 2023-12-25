9add86fed4b8ce0d9686e3ba55b5ba757cc005e0
alparusja <alparusja@mail.ru>
2023-12-25 20:50:53 +0300

    add Program.cs 2_2

--- lesson_2/2_2/Program.cs
+++ lesson_2/2_2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Напишите программу, которая принимает на вход трёхзначное
+//    число и возводит вторую цифру этого числа в степень, равную
+//    третьей цифре.
+Console.WriteLine("Впишите число: ");
+int a = int.Parse(Console.ReadLine()!);
+if (a >= 100 && a <= 1000)
+{
+    int a1 = a % 100 / 10;
+    int a2 = a % 10;
+    double result = Math.Pow(a1,a2);
+    Console.WriteLine(result);
+
+}
+else
+{
+    if (a < 100 || a > 1000)
+    {
+        Console.WriteLine("Вы ввели не трехзначное число");
+    }
+}
