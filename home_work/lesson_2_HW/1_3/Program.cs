2266a9907817016229893a06838e4d0f8b771aa7
alparusja <alparusja@mail.ru>
2023-12-27 15:36:28 +0300

    add lesson_2_HW 1_3 Решение Задачи 3

--- home_work/lesson_2_HW/1_3/Program.cs
+++ home_work/lesson_2_HW/1_3/Program.cs
@@ -1,4 +1,17 @@
 ﻿// Задача 3: Напишите программу, которая принимает на вход целое число
 // из отрезка [10, 99] и показывает наибольшую цифру числа.
-Console.WriteLine("Hello, World!");
-new Random().Next()
+Console.WriteLine("Впишите число от 10 до 99: ");
+int a = int.Parse(Console.ReadLine()!);
+int max = 0;
+
+if (a > 9 && a < 100)
+{
+    int num1 = a % 10;
+    int num2 = a / 10;
+    max = Math.Max(num1, num2);
+    Console.WriteLine(max);
+}
+else
+{
+    Console.WriteLine("Число не входит в заданный диапазон");
+}
