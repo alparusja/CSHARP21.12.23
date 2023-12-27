507abdf6751736c20e9c32e8b2515ee22ee205a0
alparusja <alparusja@mail.ru>
2023-12-27 15:25:26 +0300

    add lesson_2_HW 1_2 Решение Задачи 2

--- home_work/lesson_2_HW/1_2/Program.cs
+++ home_work/lesson_2_HW/1_2/Program.cs
@@ -1,4 +1,24 @@
 ﻿// Задача 2: Напишите программу, которая принимает на вход координаты 
 // точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной 
 // четверти плоскости, в которой находится эта точка.
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Впишите X: ");
+int X = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Впишите Y: ");
+int Y = int.Parse(Console.ReadLine()!);
+
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine("Вы находитесь в первой координатной плоскости");
+}
+else if (X < 0 && Y > 0)
+{
+    Console.WriteLine("Вы находитесь во второй координатной плоскости");
+}
+else if (X < 0 && Y < 0)
+{
+    Console.WriteLine("Вы находитесь в третьей координатной плоскости");
+}
+else if (X > 0 && Y < 0)
+{
+    Console.WriteLine("Вы находитесь в четвертой координатной плоскости");;
+}
