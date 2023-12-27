3bcbfbf99736ec74752d05a18e89f355697becdf
alparusja <alparusja@mail.ru>
2023-12-27 15:55:25 +0300

    add lesson_2_HW 1_4 Решение Задачи 4

--- home_work/lesson_2_HW/1_4/Program.cs
+++ home_work/lesson_2_HW/1_4/Program.cs
@@ -1,3 +1,19 @@
 ﻿// Задача 4: Напишите программу, которая на вход принимает натуральное
 // число N, а на выходе показывает его цифры через запятую.
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Впишите натуральное число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+string number = "";
+if (N > 0)
+{
+   while (N > 0)
+    {
+        number = N % 10 + "," + number;
+        N = N / 10;
+    }
+    Console.Write(number.Trim(',') + "\n");
+}
+else
+{
+    Console.WriteLine("Ввели не натуральное число");
+}
