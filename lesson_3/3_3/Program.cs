e9e5801c46a586846a3481031840d0cf6010c63b
alparusja <alparusja@mail.ru>
2024-01-12 21:01:56 +0300

    add lesson_3_3_3

---
+++ lesson_3/3_3/Program.cs
@@ -0,0 +1,10 @@
+﻿// 3.
+int[] numbers = { 2, 8, 3, -4, 2, -8, 1, -2, 5, 8 };
+int[] arr = new int[numbers.Length / 2];
+int i = 0;
+
+for (i = 0; i < numbers.Length / 2; i++)
+{
+    arr[i] = numbers[i] * numbers[numbers.Length - 1 - i];
+    Console.WriteLine("Результат: " + arr[i]);
+}
