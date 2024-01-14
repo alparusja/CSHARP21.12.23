88a5e3ea38095e7e1b49f575ca3ed060a15657fa
alparusja <alparusja@mail.ru>
2024-01-14 17:39:49 +0300

    add lesson_3_3_4

---
+++ lesson_3/3_4/Program.cs
@@ -0,0 +1,36 @@
+﻿// 4. Дано натуральное трёхзначное число. Создайте массив, 
+// состоящий из цифр этого числа. Младший разряд числа должен 
+// располагаться на 0-м индексе массива, старший – на 2-м/
+
+using System;
+
+namespace My_3_1
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.Write("Введите натуральное трёхзначное число: ");
+            int element = Convert.ToInt32(Console.ReadLine()!);
+
+            int[] My_mass = new int[3];
+            int set = 0;
+            if ((element > 99) && (element < 1000))
+            {
+                System.Console.Write("[ ");
+                while (element > 0)
+                {
+                    My_mass[set] = element % 10;
+                    element = element / 10;
+                    Console.Write(My_mass[set] + " ");
+                    set++;
+                }
+                System.Console.Write("]");
+                System.Console.WriteLine();
+            }
+            else
+                Console.WriteLine("Неверные данные");
+        }
+    }
+}
