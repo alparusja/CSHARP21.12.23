462f922c7b2fd1178bf4e12b4e2c18268cc590fc
alparusja <alparusja@mail.ru>
2024-01-14 17:26:34 +0300

    add lesson_3_3_3 Решение с методом

--- lesson_3/3_3/Program.cs
+++ lesson_3/3_3/Program.cs
@@ -11,3 +11,45 @@ for (i = 0; i < numbers.Length / 2; i++)
     arr[i] = numbers[i] * numbers[numbers.Length - 1 - i];
     Console.WriteLine("Результат: " + arr[i]);
 }
+
+// Решение с Методом на C#
+void Print(int[] arr)
+{
+int size = arr.Length;
+
+for (int i = 0; i < size; i++)
+Console.Write($"{arr[i]} ");
+Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+int[] arr = new int[size];
+
+for (int i = 0; i < size; i++)
+arr[i] = new Random().Next(from, to + 1);
+return arr;
+}
+
+int[] PairsNum(int[] arr)
+{
+int size = arr.Length;
+int flex_size = size / 2 + size % 2;
+int[] new_arr = new int[flex_size];
+
+for (int i = 0; i < size / 2; i++)
+new_arr[i] = arr[i] * arr[size - i - 1];
+
+if (size % 2 == 1)
+new_arr[flex_size - 1] = arr[size / 2];
+return new_arr;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+int[] arr_new = PairsNum(mass);
+Print(arr_new);
