c54f9d96c6e3c52dae96a48256d2bb5ed2f1e47e
alparusja <alparusja@mail.ru>
2024-01-15 21:54:09 +0300

    add lesson_4 4_1 шаблоны

---
+++ lesson_4/4_1/Program.cs
@@ -0,0 +1,42 @@
+﻿// 1. Задайте одномерный массив, заполненный случайными
+//    числами. Определите количество простых чисел в этом массиве.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to + 1);
+    return arr;
+}
+
+int InRange(int[] arr)
+{   
+    int n = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 20 && arr[i] <= 90)        
+            n += 1;          
+    }
+    return n;    
+}
+
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+Console.WriteLine(InRange(mass));
