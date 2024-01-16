dde84f105dd326c76b0b1d925bb3a93580254d7b
alparusja <alparusja@mail.ru>
2024-01-16 16:52:26 +0300

    add lesson_4 4_2

---
+++ lesson_4/4_2/Program.cs
@@ -0,0 +1,45 @@
+﻿// // 2. Задайте массив из N случайных целых
+// чисел (N вводится с клавиатуры).  Найдите количество чисел, 
+// которые оканчиваются на 1 и делятся нацело на 7.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+    
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to + 1);
+    return arr;
+}
+
+int Num(int[] arr)
+{
+    int n = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 7 == 0 && arr[i] % 10 == 1 )
+        {
+            n++;
+        }
+    }
+    return n;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+int result = Num(mass);
+Console.WriteLine(result);
