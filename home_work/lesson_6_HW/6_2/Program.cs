a30889c592efca2c97e3a1106e7932ff54e1225f
alparusja <alparusja@mail.ru>
2024-02-07 11:27:09 +0300

    add lesson_6_HW 6_2

---
+++ home_work/lesson_6_HW/6_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы
+// в обоих регистрах. Сформируйте строку, в которой все
+// заглавные буквы заменены на строчные. 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Задайте произвольную строку из латинских букв (заглавные и строчные): ");
+        string s = Console.ReadLine()!;
+        string Word = WordToLower(s);
+    }
+    static string WordToLower(string s)
+    {
+        string s2 = new string(s);
+
+        Console.WriteLine(s2.ToLower());
+        return s2;
+    }
+}
