e90264350d082c3580a33cc6a4ad62b0408b4371
alparusja <alparusja@mail.ru>
2024-02-07 12:42:24 +0300

    add lesson_7_HW 7_3

---
+++ home_work/lesson_7_HW/7_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        int[] array = { 8, 6, 58, 102, 85 };
+        Array.Reverse(array);
+        PrintArray(array);
+    }
+    static void PrintArray(int[] array, int i = 0)
+    {
+        if (i < array.Length)
+        {
+            Console.Write(array[i] + "\t");
+            PrintArray(array, i + 1);
+        }
+    }
+}
