254a51921fe880d546f5aff3e58b72e33dfc6862
alparusja <alparusja@mail.ru>
2024-02-07 11:52:07 +0300

    add lesson_7_HW 7_2

---
+++ home_work/lesson_7_HW/7_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("m = ");
+        int m = Convert.ToInt32(Console.ReadLine()!);
+        System.Console.WriteLine("n = ");
+        int n = Convert.ToInt32(Console.ReadLine()!);
+        Console.WriteLine(Ackerman(m, n));
+    }
+    static int Ackerman(int m, int n)
+    {
+        if (m < 0 || n < 0) throw new ArgumentOutOfRangeException();
+        if (m == 0) return n + 1;
+        if (n == 0) return Ackerman(m - 1, 1);
+        return Ackerman(m - 1, Ackerman(m, n - 1));
+    }
+}
