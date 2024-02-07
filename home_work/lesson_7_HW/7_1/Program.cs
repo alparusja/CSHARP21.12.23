2017cd2d6415ee5ad12cdf63eb57dd3b7431c6be
alparusja <alparusja@mail.ru>
2024-02-07 11:44:00 +0300

    add lesson_7_HW 7_1

---
+++ home_work/lesson_7_HW/7_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("M =  ");
+        int M = Convert.ToInt32(Console.ReadLine()!);
+        System.Console.WriteLine("N = ");
+        int N = Convert.ToInt32(Console.ReadLine()!);
+        Interval(M, N);
+    }
+    static int Interval(int M, int N)
+    {
+        if (M <= N)
+        {
+            Console.Write(M + " ");
+            M++;
+            Interval(M, N);
+        }
+        return M;
+    }
+}
